17bf63de55ccd97d3cdc8842cb40341b7b621cc7
prateeksingh132 <kumarpks132@gmail.com>
2025-12-16 12:04:05 -0700

    Part 1-3 done

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+
+// Part 1
+
+let mainEl = document.querySelector('main');
+
+// we need var() function to access the root variaable with '--': https://www.w3schools.com/css/css3_variables_javascript.asp
+mainEl.style.backgroundColor = 'var(--main-bg)';
+
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+
+mainEl.classList.add('flex-ctr');
+
+
+// Part 2
+
+let topMenuEl = document.getElementById('top-menu');
+
+topMenuEl.style.height = '100%';
+
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+
+topMenuEl.classList.add('flex-around');
+
+
+// Part 3
+
+// Menu data structure
+var menuLinks = [
+  { text: 'about', href: '/about' },
+  { text: 'catalog', href: '/catalog' },
+  { text: 'orders', href: '/orders' },
+  { text: 'account', href: '/account' },
+];
+
+// it doesnt work for for..in .. have to use forEach, find out why?
+menuLinks.forEach((link) => {
+    let aElement = document.createElement('a');
+    aElement.setAttribute('href', link.href);
+    aElement.textContent = link.text;
+    topMenuEl.appendChild(aElement);
+});
